0e8b2f78e69928d50ee076b10e6ffd51426cd699
malesive <june801@June801deMac-mini.local>
2016-01-20 14:35:40 +0800

    ch1 finished

--- Quiz/Quiz/ViewController.swift
+++ Quiz/Quiz/ViewController.swift
@@ -13,13 +13,42 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        questionLabel.text = questions[currentQuestionIndex]
+    }
+//
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
+
+
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    @IBAction func showNextQuestion(sender: AnyObject) {
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        
+        let question = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func showAnswer(sender: AnyObject) {
+        let answer = answers[currentQuestionIndex]
+        answerLabel.text = answer
     }
-
-
+    
+    let questions = ["火星的英文名是啥？",
+                    "7+7等于多少呀？",
+                    "台湾的省会是那里呀？", ]
+    let answers = ["Mars", "14", "台北"]
+    
+    var currentQuestionIndex = 0
+    
+    
 }
 
