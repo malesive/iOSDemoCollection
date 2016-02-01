4c839f8661c465e941a3b5cfea6d3909736301b5
malesive <june801@June801deMac-mini.local>
2016-02-01 17:45:05 +0800

    当时测试TouchID的

---
+++ iOSTouchIDDemo/iOSTouchIDDemo/iOSTouchIDDemo/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Demo20150906
+//
+//  Created by June801 on 15/9/6.
+//  Copyright (c) 2015年 June801. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
