89a1111b716bbe8b5ef04c304b31ba5a06f49baa
malesive <june801@June801deMac-mini.local>
2016-04-08 10:46:21 +0800

    effective objective-c 2.0 demo

---
+++ EOCProject/EOCProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EOCProject
+//
+//  Created by June801 on 16/4/8.
+//  Copyright © 2016年 June801. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ EOCProject/EOCProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EOCProject
+//
+//  Created by June801 on 16/4/8.
+//  Copyright © 2016年 June801. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
